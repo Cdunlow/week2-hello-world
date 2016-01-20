3d0f9c227eeab88ab4fac4c8387359f96d5c4730
Charles Dunlow <cdunlow@mail.valenciacollege.edu>
2016-01-20 17:15:35 -0500

    Redone
    added everything as well as i could

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,21 @@
-# replace the contents of this comment with your full name
+# Charles Dunlow
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+import sys #allows sys commands
 
-# make sure that your code contains comments explaining your logic!
-
+print('Hello World!') #greets the world in english
+print('Choose a language and I will greet you in that language!') #ask the user to choose a language from a list i provided
+print('1 for English') #prints out 1 for english
+print('2 for French') #prints out 2 for french
+print('3 for Spanish')   #prints out 3 for spanish
+chosenLanguage = input()   #will recognize what the user inputed
+if chosenLanguage == '1':   #will print in english if chosen
+   print('Hello world!')
+if chosenLanguage == '2':
+   print('Bonjour le monde!')   #will print in french if chosen
+if chosenLanguage == '3':   #will print in spanish if chosen
+   print('¡Hola Mundo!')
+while True:      #is used to exit the program
+    print(' Type Q to exit the program.')    #prints out statement for user to acknowledge
+    response = input()   #recognizes what user inputed
+    if response == 'Q':   #makes sure the user only entered "Q" otherwie ask user again for correct response
+        sys.exit()   #exits program completley
